b6134b3c062f5508e0bf72b5433e0c8654191a23
nalysnyk <nalysnyk@gmail.com>
2018-07-14 12:18:04 +0200

    Optimized algorithm

--- src/main/java/test/task/service/impl/RainyHillsServiceImpl.java
+++ src/main/java/test/task/service/impl/RainyHillsServiceImpl.java
@@ -5,8 +5,6 @@ import test.task.service.RainyHillsService;
 import javax.enterprise.context.RequestScoped;
 import java.math.BigInteger;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Objects;
 
 /**
@@ -35,66 +33,40 @@ public class RainyHillsServiceImpl implements RainyHillsService {
 
         BigInteger volume = BigInteger.ZERO;
 
-        int maxVal = Arrays.stream(array).mapToInt(i -> i).max().getAsInt();
-        int maxIndex = Arrays.asList(array).indexOf(maxVal);
+        Integer leftIndex = 0;
+        Integer rightIndex = array.length - 1;
 
-        Integer[] leftRange = Arrays.copyOfRange(array, 0, maxIndex + 1);
-        volume = volume.add(calculateVolumeForRange(leftRange, maxVal, true));
+        Integer leftValue = array[leftIndex];
+        Integer rightValue = array[rightIndex];
 
-        Integer[] rightRange = Arrays.copyOfRange(array, maxIndex, array.length);
-        volume = volume.add(calculateVolumeForRange(rightRange, maxVal, false));
+        while(!leftIndex.equals(rightIndex)) {
 
-        return volume;
-    }
+            if (leftValue >= rightValue) {
 
-    private BigInteger calculateVolumeForRange(Integer[] array, int maxVal, boolean isLeft) {
-        int minForRange = Arrays.stream(array).mapToInt(i -> i).min().orElse(maxVal);
-        BigInteger volume = BigInteger.ZERO;
-        if (minForRange != maxVal) {
-
-            int ariaMax = Arrays.stream(
-                    Arrays.copyOfRange(array, (isLeft ? 0 : 1), (isLeft ? (array.length - 1) : array.length)))
-                    .mapToInt(i -> i)
-                    .max()
-                    .orElse(maxVal);
-
-            int ariaMaxIndex = indexOfWithExclude(array, ariaMax, isLeft,
-                    Collections.singletonList((isLeft ? (array.length - 1) : 0)));
-
-            if (ariaMaxIndex == -1) {
-                return volume;
-            }
-            volume = volume.add(calculateWaterAmount(ariaMax, Arrays.copyOfRange(
-                    array, (isLeft ? ariaMaxIndex : 0), (isLeft ? array.length : ariaMaxIndex))));
-            volume = volume.add(calculateVolumeForRange(Arrays.copyOfRange(
-                    array, (isLeft ? 0 : ariaMaxIndex), (isLeft ? (ariaMaxIndex + 1) : array.length)), ariaMax, isLeft));
-        }
-
-        return volume;
-    }
-
-    private BigInteger calculateWaterAmount(Integer minLevel, Integer[] array) {
-        BigInteger volume = BigInteger.ZERO;
-        for (Integer anArray : array) {
-            BigInteger result = BigInteger.valueOf(minLevel).subtract(BigInteger.valueOf(anArray));
-            if (result.compareTo(BigInteger.ZERO) > 0) {
-                volume = volume.add(result);
-            }
-        }
-        return volume;
-    }
-
-    private int indexOfWithExclude(Integer[] array, Integer val, boolean last, List<Integer> excludes) {
-        int index = -1;
-        for (int i = 0; i < array.length; i++) {
-            if (val.equals(array[i]) && !excludes.contains(i)) {
-                if (last) {
-                    index = i;
+                if (array[rightIndex - 1] < rightValue) {
+                    volume = volume.add(
+                            BigInteger.valueOf(rightValue).subtract(BigInteger.valueOf(array[--rightIndex])));
                 } else {
-                    return i;
+                    rightIndex--;
+                    rightValue = array[rightIndex];
                 }
+
+            } else {
+
+                if (array[leftIndex + 1] < leftValue) {
+                    volume = volume.add(
+                            BigInteger.valueOf(leftValue).subtract(BigInteger.valueOf(array[leftIndex + 1])));
+                    leftIndex++;
+                } else {
+                    leftIndex++;
+                    leftValue = array[leftIndex];
+                }
+
             }
+
         }
-        return index;
+
+        return volume;
     }
+
 }
